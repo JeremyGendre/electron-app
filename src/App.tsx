89e9c989f525423019ba5bfc00d7d2baee972936
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-07-03 17:20:48 +0200

    feat: listen scroll and add style on header

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import {getCurrentDir, getFilesFromPath} from "./utils/File.service";
 import {useFileContext} from "./contexts/FileContext";
 import Input from "./components/Input/Input";
 import Button from "./components/Button/Button";
+import {useScroll} from "@jeremygendre/react-custom-hooks";
 
 function App() {
     const [searchedValue, setSearchedValue] = useState(getCurrentDir('.'));
     const {path, setPath} = useFileContext();
     const files = useMemo(() => getFilesFromPath(path),[path]);
+
+    const {scrollY} = useScroll();
 
     useEffect(() => {
         if(searchedValue !== path) setSearchedValue(path);
@@ -21,11 +24,15 @@ function App() {
 
     return (
         <div>
-            <form onSubmit={handleSubmit} className="mx-10 my-2 flex space-x-2">
-                <Input label="Browse :" value={searchedValue} autoFocus onChange={e => setSearchedValue(e.currentTarget.value)}/>
-                <Button className="mt-auto" disabled={searchedValue === path} type="submit">Search</Button>
-            </form>
-            <hr className="my-4"/>
+            <div className="sticky top-0">
+                <form onSubmit={handleSubmit} className={`px-10 py-2 flex bg-white space-x-2 ${scrollY > 0 ? 'shadow-md' : ''}`}>
+                    <Input label={scrollY > 0 ? undefined : 'Browse :'} value={searchedValue} autoFocus
+                           onChange={e => setSearchedValue(e.currentTarget.value)}
+                    />
+                    <Button className="mt-auto" disabled={searchedValue === path} type="submit">Search</Button>
+                </form>
+                <hr className="mb-4"/>
+            </div>
             <FileViewer files={files}/>
         </div>
     )
