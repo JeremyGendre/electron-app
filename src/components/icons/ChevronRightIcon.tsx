4ea30aec934a95a609a686eda3c1c238844239c4
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-07-03 18:26:46 +0200

    feat: fix format size & add chevron

---
+++ src/components/icons/ChevronRightIcon.tsx
@@ -0,0 +1,8 @@
+export default function ChevronRightIcon(){
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" style={{width: '1em', height: '1em'}} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+    );
+}
+
--- src/utils/File.service.ts
+++ src/utils/File.service.ts
@@ -6,6 +6,7 @@ export const getCurrentDir = (path: string) => pathModule.resolve(path);
 export const getParentDir = (path: string) => pathModule.dirname(pathModule.resolve(path));
 
 export const formatSize = (size: number) => {
+    if(size <= 0) return '0 B';
     const i :number = Math.floor(Math.log(size) / Math.log(1024));
     return (
         (size / Math.pow(1024, i)).toFixed(2) +
